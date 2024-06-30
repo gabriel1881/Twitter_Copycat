57dc93c9af660f77e509a6499cbb78813b0e8c65
gabriel1881 <ggabriel1881@gmail.com>
2024-06-30 18:11:56 -0300

    Constants folder & svg files

---
+++ twitter_copycat/lib/constants/appwrite_constants.dart
@@ -0,0 +1,5 @@
+class AppwriteConstants {
+  static const String databaseId = 'twitter_database';
+  static const String projectId = '6681850100271f51474d';
+  static const String endPoint = 'http://localhost:80/v1';
+}
