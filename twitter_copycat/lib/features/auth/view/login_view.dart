598f253779234ed410556a112b3d904c8b87e888
gabriel1881 <ggabriel1881@gmail.com>
2024-07-01 15:07:07 -0300

    Login_view screen

---
+++ twitter_copycat/lib/features/auth/view/login_view.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:twitter_copycat/features/auth/widgets/widgets.dart';
+
+class LoginView extends StatefulWidget{
+  const LoginView({super.key});
+
+  @override
+  State<LoginView> createState() => _LoginViewState();
+}
+
+class _LoginViewState extends State<LoginView> {
+  @override 
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            CommonTwitterHeader(),
+            Space(height: 150),
+            LargeText(text: "See what's happening in the world right now."),
+            Space(height: 150),
+            GoogleButton(),
+            Space(height: 5),
+            OrLine(),
+            Space(height: 5),
+            CreateAccountButton(),
+            Space(height: 20),
+            SmallText(),
+            Space(height: 80),
+            LoginText(),
+          ],
+        ),
+      ),
+    );
+  }
+}
